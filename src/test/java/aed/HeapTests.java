d729eb82461022152ec4140cf13b5b02d42c8fa5
nadina <solernadina@gmail.com>
2024-11-04 17:06:55 -0300

    primeros archivos

---
+++ src/test/java/aed/HeapTests.java
@@ -0,0 +1,61 @@
+package aed;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class HeapTests {
+    
+    @Test
+    void heapVacio() {
+        Heap h = new Heap();
+
+        assertTrue(h.Vacio());  // Cambié a assertTrue para comprobar que el heap está vacío
+    }
+    
+    @Test
+    void obtenerMaximo(){
+        Heap h = new Heap();
+
+        h.Agregar(7);
+        h.Agregar(2);
+        h.Agregar(3);
+        h.Agregar(6);
+
+        assertEquals(7, h.Maximo());
+    }
+    
+    @Test
+    void obtenerMaximoDelVacio(){
+        Heap h = new Heap();
+
+        assertEquals(0, h.Maximo());
+    }
+
+    @Test
+    void obtenerMaximoLuegoDeQuitarMaximo(){
+        Heap h = new Heap();
+
+        h.Agregar(7);
+        h.Agregar(2);
+        h.Agregar(3);
+        h.Agregar(6);
+        h.SacarMaximo();
+
+        assertEquals(6, h.Maximo());
+    }
+
+    @Test
+    void obtenerMaximoDeTodosNegativos(){
+        Heap h = new Heap();
+
+        h.Agregar(-7);
+        h.Agregar(-3);
+        h.Agregar(-15);
+        h.Agregar(-2);
+        h.Agregar(-24);
+        h.Agregar(-1);
+
+        assertEquals(-1, h.Maximo());
+    }
+}
