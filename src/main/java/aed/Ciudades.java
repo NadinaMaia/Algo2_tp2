018a7395589655908c1262bbb98845a1cfb57884
LNBiasoni <talibiasoni01@gmail.com>
2024-11-08 23:42:29 -0300

    .

--- src/main/java/aed/Ciudades.java
+++ src/main/java/aed/Ciudades.java
@@ -11,8 +11,8 @@ public class Ciudades {
     int tamañoP;
 
     public Ciudades(ArrayList<Ciudad> arr) {
-        mayorGanancia = new ArrayList<Ciudad>(arr.size());
-        mayorPerdida = new ArrayList<Ciudad>(arr.size());
+        mayorGanancia = new ArrayList<Integer>(arr.size());
+        mayorPerdida = new ArrayList<Integer>(arr.size());
         tamañoG=0;
         tamañoP=0;
         mayorSuperavit = new Heap<>(arr, new ComparadorSuperavit(),2);
@@ -21,18 +21,18 @@ public class Ciudades {
 
     public void actualizarMayorGanancia(Ciudad ciudadOrigen){
         if (tamañoG==0){
-                    mayorGanancia.add(ciudadesArray.get(max.origen).id);
+                    mayorGanancia.add(ciudadOrigen.id);
                     tamañoG ++;
                 }
                 else{
-                    Ciudad ciudadOrigen =  ciudadesArray.get(max.origen);
+                    
                     int ciudadG = mayorGanancia.get(0);
                     if (ciudadOrigen.ganancia == ciudadesArray.get(ciudadG).ganancia){
                         mayorGanancia.add(ciudadOrigen.id);
                         tamañoG ++;
                     }
-                    else if (ciudadOrigen.ganancia> ciudades.mayorGanancia.get(0).ganancia){
-                        mayorGanancia = new ArrayList<int>(ciudades.ciudadesArray.size());
+                    else if (ciudadOrigen.ganancia> ciudadesArray.get(ciudadG).ganancia){
+                        mayorGanancia = new ArrayList<Integer>(ciudadesArray.size());
                         mayorGanancia.add(ciudadOrigen.id);
                         tamañoG = 1;
 
@@ -42,31 +42,30 @@ public class Ciudades {
     } 
 
     public void actualizarMayorPerdida(Ciudad ciudadDestino){
-        if (ciudades.tamañoP==0){
-                    ciudades.mayorPerdida.add(ciudades.ciudadesArray.get(max.destino).id);
-                    ciudades.tamañoG ++;
+        if (tamañoP==0){
+                    mayorPerdida.add(ciudadDestino.id);
+                    tamañoG ++;
                 }
                 else{
-                    Ciudad ciudadDestino =  ciudades.ciudadesArray.get(max.destino);
-                    int ciudadG = ciudades.mayorGanancia.get(0);
-                    if (ciudadDestino.perdida == ciudades.ciudadesArray.get(ciudadG).perdida){
-                        ciudades.mayorPerdida.add(ciudadDestino.id);
-                        ciudades.tamañoG ++;
+                    int ciudadP = mayorGanancia.get(0);
+                    if (ciudadDestino.perdida == ciudadesArray.get(ciudadP).perdida){
+                        mayorPerdida.add(ciudadDestino.id);
+                        tamañoG ++;
                     }
-                    else if (ciudadDestino.ganancia> ciudades.mayorGanancia.get(0).perdida){
-                        ciudades.mayorPerdida = new ArrayList<int>(ciudades.ciudadesArray.size());
-                        ciudades.mayorPerdida.add(ciudadDestino.id);
-                        ciudades.tamañoG = 1;
+                    else if (ciudadDestino.ganancia> ciudadesArray.get(ciudadP).perdida){
+                        mayorPerdida = new ArrayList<Integer>(ciudadesArray.size());
+                        mayorPerdida.add(ciudadDestino.id);
+                        tamañoG = 1;
 
                     }
 
                 }
     }
-    public ArrayList<Ciudad> obtenerMayorGanancia(){ //O(1)
+    public ArrayList<Integer> obtenerMayorGanancia(){ //O(1)
         return mayorGanancia;
     }
 
-    public ArrayList<Ciudad> obtenerMayorPerdida(){ //O(1)
+    public ArrayList<Integer> obtenerMayorPerdida(){ //O(1)
         return mayorPerdida;
     }
 
@@ -75,6 +74,6 @@ public class Ciudades {
     }
 
     public ArrayList<Ciudad> obtenerCiudades(){ //O(1)
-        return ciudades;
+        return ciudadesArray;
     }
 }
